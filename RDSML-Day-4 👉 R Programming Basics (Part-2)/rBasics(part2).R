7cd8f73cfd5d6378d7dad5ef521f0611d9643167
raziasultanaruman <raziaruman77@gmail.com>
2025-05-17 03:01:09 +0600

    Day 2 file

---
+++ RDSML-Day-4 👉 R Programming Basics (Part-2)/rBasics(part2).R
@@ -0,0 +1,44 @@
+# Data Types in R
+# 1. Numeric- Integer / Floating type
+# 2. String/ Character - "ruman" / 'ruman'
+# logical 
+
+a = 11
+b = 7.19
+class(a)
+class(b)
+
+name = "razia ruman"
+print(name)
+class(name)
+
+c = "52"
+print(c+9)
+print(a+9)
+class(c)
+#Logical data type - (T, F)
+class(a > b)
+print(a< b)
+x = 19
+y = 22-3
+print(x==y)
+print(x!=y)
+print(y >= x)
+
+
+#Logical operators in R
+# && logical operator
+# || logical operator
+4 == 4 && 6 == 6
+x != y || x == y
+
+# Simple mathematical operator
+
+w = (4 + 3 - 3) * 2 / 3
+print(w) 
+y = 2^4
+print(y)
+
+11/5
+11 %% 5
+11 %/% 5
