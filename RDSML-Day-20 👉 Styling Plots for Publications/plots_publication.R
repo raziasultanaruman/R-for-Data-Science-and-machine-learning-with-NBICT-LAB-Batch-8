5a2d90fb96d7701bb16534673e92ae20f66c870c
raziasultanaruman <raziaruman77@gmail.com>
2025-07-11 01:36:09 +0600

    publishable plot

---
+++ RDSML-Day-20 👉 Styling Plots for Publications/plots_publication.R
@@ -0,0 +1,33 @@
+# Styling plots for publications
+
+library(ggplot2)
+#install.packages('ggthemes')
+library(ggthemes)
+#install.packages("viridis")
+library(viridis)
+
+# Sample data loading
+df = mtcars
+df$cyl = as.factor(df$cyl)
+
+# Styling plot
+p = ggplot(df, aes(x = wt, y = mpg, color = cyl)) +
+  geom_point(size = 3) +
+  scale_color_viridis_d(name = "cylinders") +
+  labs(
+    title = "Fuel Efficiency by car weight",
+    x = "weight(1000 lbs)",
+    y = "Miles per gallon"
+  ) +
+  theme_classic(base_size = 14, base_family = "serif") +
+  theme(plot.title = element_text(face = "bold", size = 16, hjust = 0.5),
+        axis.title = element_text(face = "bold", size = 14),
+        axis.text = element_text(size = 14),
+        legend.position = "right",
+        legend.title = element_text(face = "bold"),
+        legend.text = element_text(size = 12)
+        )
+
+# Save the plot for publications
+ggsave("publication.ggplot.png", p, width = 6, height = 4, dpi =300)
+
